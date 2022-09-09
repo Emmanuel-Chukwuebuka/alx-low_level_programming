817af558478847a36afe36940a68cfb69c33a5fe
Emmanuel-Chukwuebuka <Chukwuebukan042@gmail.com>
2022-09-08 18:38:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  *main - entry function
  * Return: 0 after printinf function 
  */
 int main (void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

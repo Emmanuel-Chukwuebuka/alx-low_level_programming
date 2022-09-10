b0c66dc9188a4b5832415e48c4b9284179330e2d
Emmanuel-Chukwuebuka <Chukwuebukan042@gmail.com>
2022-09-09 21:09:13 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers in base 10
+ *
+ * Return: true
+ */
+int main(void)
+{
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('/n');
+
+	return (0);
+}

8c06307d0d0ad691802c622b35b05b0522df68be
Emmanuel-Chukwuebuka <Chukwuebukan042@gmail.com>
2022-09-14 19:44:04 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+nclude "main.h"
+/**
+ * print_sign - functons prints and check for a number sign
+ * @n: integer used for the function
+ * Return: successful
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1)
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else 
+	{
+		_putchar('0');
+		return (0);
+	}
+}

879270d7507913e8085b2087a7c29a1fda63d60e
Emmanuel-Chukwuebuka <Chukwuebukan042@gmail.com>
2022-09-14 18:55:14 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
- * 2-print_alphabet_x10.c - function prints alphabets 10X
+ * print_alphabet_x10.c - function prints alphabets 10X
  *
  * Return: successful
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	char i;
-	
+	char c, i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)

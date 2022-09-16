d947080935742110b93204badd4eee013c8f7101
Emmanuel-Chukwuebuka <Chukwuebukan042@gmail.com>
2022-09-15 20:24:12 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all numbers
+ * Return: null
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
